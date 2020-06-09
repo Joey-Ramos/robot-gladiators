9f781bab9a290cf673cbd8f799095ad513a0434a
Joey Ramos <jramos1202@gmail.com>
2020-06-08 22:23:30 -0500

    required to get player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,7 +65,6 @@ var startGame = function() {
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
       var pickedEnemyObj = enemyInfo[i];
 
       pickedEnemyObj.health = randomNumber(40, 60);
@@ -147,8 +146,19 @@ var randomNumber = function(min, max) {
 
   return value;
 };
+// function to set name
+var getPlayerName = function() {
+  var name = "";
 
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
 var playerInfo = {
+  name: getPlayerName(),
   name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
